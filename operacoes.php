d216200929cabec7167faf6519b48f77db78759d
MrHighTech20 <marcoscantelli@hotmail.com>
2023-01-09 11:23:48 -0300

    operações matemáticas básicas

---
+++ operacoes.php
@@ -0,0 +1,18 @@
+<?php
+
+// Realizando operações matemáticas
+
+//Soma
+$idade = 21;
+$idadeSoma = $idade + 10;
+echo $idadeSoma;
+
+/*Subtração
+$anoAtual = 2023;
+$anoNascimento = 2002;
+$idadeAtual = $anoAtual - $anoNascimento;
+echo $idadeAtual;
+
+$multiplicacao = 2 * 2;
+
+$divisao = 2 / 2;*/
