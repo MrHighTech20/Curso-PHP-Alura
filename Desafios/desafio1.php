7f46816e89d831bc17d1f2a2bb428ff2ba96b91b
MrHighTech20 <marcoscantelli@hotmail.com>
2023-01-13 10:11:18 -0300

    desafio de exibir apenas números impares

---
+++ Desafios/desafio1.php
@@ -0,0 +1,16 @@
+<?php
+
+// Desafio 1 EXIBINDO NÚMEROS IMPÁRES ATÉ 100
+
+for ($contador = 1; $contador <= 100; $contador ++) {
+
+    if ($contador % 2 != 0){
+
+    } else {
+        $contador != 0;
+        echo "#$contador" . PHP_EOL;
+    }
+    
+    
+    
+}
