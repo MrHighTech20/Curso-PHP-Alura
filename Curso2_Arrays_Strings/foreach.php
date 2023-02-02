4a2ca40b1c11aa848613d6c02dbcd279bccbece0
MrHighTech20 <marcoscantelli@hotmail.com>
2023-02-02 17:23:04 -0300

    modificando informação de CPF

--- Curso2_Arrays_Strings/foreach.php
+++ Curso2_Arrays_Strings/foreach.php
@@ -1,27 +1,31 @@
 <?php
 
-$conta1 = [
-    'titular' => 'Vinicius', 
-    'saldo' => 1000
-];
-
-$conta2 = [
-    'titular' => 'Maria', 
-    'saldo' => 1000
-];
-
-$conta3 = [
-    'titular' => 'Alberto', 
-    'saldo' => 300
-];
-
 $contasCorrentes = [
-    12345678910 => $conta1, 
-    12345648911 => $conta2, 
-    12325678910 => $conta3
+    '123.456.789-10' => [
+        'titular' => 'Maria', 
+        'saldo' => 10000
+    ],
+    '123.456.489-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-10' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+$contasCorrentes['123.258.852-12'] = [
+    'titular' => 'Claudia',
+    'saldo' => 2000
+];
+
+$contasCorrentes[] = [
+    'titular' => 'Fabio',
+    'saldo' => 4002
 ];
 
 foreach ($contasCorrentes as $cpf => $conta){
-    echo $cpf['titular'] . PHP_EOL;
+    echo $cpf . " " . ['titular'] . PHP_EOL;
 }
 
--- Curso2_Arrays_Strings/listas.php
+++ Curso2_Arrays_Strings/listas.php
@@ -1,7 +1,10 @@
 <?php
 
 $idadeList = [20, 22, 18, 23, 30, 40, 19];
-$exibirIdade = $idadeList[5];
 
-echo $exibirIdade;
+$idadeList[] = 20;
+
+foreach ($idadeList as $idade){
+    echo $idade . PHP_EOL;
+}
 
