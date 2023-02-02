6e9783f066c40834fc024c17b1760ca8617ed369
MrHighTech20 <marcoscantelli@hotmail.com>
2023-02-02 17:57:00 -0300

    criando processo de banco

---
+++ Curso2_Arrays_Strings/banco.php
@@ -0,0 +1,20 @@
+<?php
+
+$contasCorrentes = [
+    '123.456.789-10' => [
+        'titular' => 'Maria', 
+        'saldo' => 10000
+    ],
+    '123.456.489-11' => [
+        'titular' => 'Alberto',
+        'saldo' => 300
+    ],
+    '123.256.789-10' => [
+        'titular' => 'Vinicius',
+        'saldo' => 100
+    ]
+];
+
+foreach ($contasCorrentes as $cpf => $conta){
+    echo $cpf . " " . ['titular'] . PHP_EOL;
+}
