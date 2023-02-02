d0f7a61e53edf27c68b0c69a1dd065b20464d094
MrHighTech20 <marcoscantelli@hotmail.com>
2023-02-02 18:07:43 -0300

    condição de saque

--- Curso2_Arrays_Strings/banco.php
+++ Curso2_Arrays_Strings/banco.php
@@ -17,6 +17,12 @@ $contasCorrentes = [
 
 $contasCorrentes['123.456.789-10']['saldo'] -= 500;
 
+if (500 > $contasCorrentes['123.456.489-11']['saldo']) {
+    echo "Você não pode sacar esse valor" . PHP_EOL;
+} else {
+    $contasCorrentes['123.456.489-11']['saldo'] -= 500;
+}
+
 foreach ($contasCorrentes as $cpf => $conta){
     echo $cpf . " " . $conta['titular'] . ' ' . $conta['saldo'] .PHP_EOL;
 }
